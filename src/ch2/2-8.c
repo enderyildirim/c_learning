f6fb600b9fb0d7774fe57e8a506a13fbe6044f1f
Ender Yildirim <enderyildirim@outlook.com>
2018-04-12 18:18:10 +0300

    Exercise 2-8.

---
+++ src/ch2/2-8.c
@@ -0,0 +1,27 @@
+/*
+ * The C Programming Language Second Edition 
+ * Exercise 2-8 answer 
+ *
+ *  Created on: Apr 12, 2018
+ *      Author: Ender Yildirim
+ *        Mail: enderyildirim@outlook.com
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void rightrot(int* x, int n) {
+	unsigned int mask = 0xffffffff;
+	mask = mask<<n;
+	int leftRemaining = *x & mask;
+	*x >>= n;
+	*x |= leftRemaining;
+}
+
+int main(void) {
+	int x = 0xff;
+	rightrot(&x, 4);
+	printf("%d", x);
+	return EXIT_SUCCESS;
+}
+
