8230d5fd5db64fd9eafb426f5a46325698f8f0cc
Ender Yildirim <enderyildirim@outlook.com>
2018-05-14 23:42:15 +0300

    3-1 fix.

--- src/ch3/3-1.c
+++ src/ch3/3-1.c
@@ -29,12 +29,17 @@ int binsearch(int x, int v[], int n) {
 
 int main(void) {
 	int size = 16;
+	          /*
+	             0, 1, 2, 3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14,  15
+	             |  |  |  |   |   |   |   |   |   |   |   |   |   |   |    |  */
 	int v[] = { -1, 2, 7, 9, 13, 17, 21, 27, 29, 38, 46, 48, 55, 72, 94, 115 };
+	printf("%d\n", binsearch(-1, v, size));
 	printf("%d\n", binsearch(9, v, size));
 	printf("%d\n", binsearch(-9, v, size));
 	printf("%d\n", binsearch(-19, v, size));
 	printf("%d\n", binsearch(55, v, size));
 	printf("%d\n", binsearch(94, v, size));
+	printf("%d\n", binsearch(115, v, size));
 	return EXIT_SUCCESS;
 }
 
